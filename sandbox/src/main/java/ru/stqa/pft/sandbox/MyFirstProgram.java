5b31459f2636c66f4f4dde088976d08b7a362d36
TanyaGo <shcherbyna.ta@gmail.com>
2021-03-09 22:37:59 -0700

    created package for the first program

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,22 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        hello("world");
+        hello("user");
+        hello("Tanya");
+
+        Square s = new Square(5);
+        System.out.println("S of square with the side" + s.l + "=" + s.area());
+
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("S of rectange with sides " + r.a + " and " + r.b + " = " + r.area());
+    }
+
+    public static void hello(String somebody){
+
+        System.out.println("Hello," + somebody+ "!");
+
+}
+}
